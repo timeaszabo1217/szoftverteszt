cae88613386b484cafb45e6e5b332259cccdae4a
Szabó Tímea <timeaszabo1217@gmail.com>
2024-10-27 21:40:51 +0100

    tests folder, new libs

---
+++ tests/tests/Server/AccountTest.java
@@ -0,0 +1,26 @@
+package tests.Server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.dataCenter.models.account.Account;
+import server.dataCenter.models.account.TempAccount;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountTest {
+
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        TempAccount tempAccount = mock(TempAccount.class);
+        when(tempAccount.getUsername()).thenReturn("testUser");
+
+        account = new Account(tempAccount);
+    }
+
+    @Test
+    public void testGetUsername() {
+        assertEquals("testUser", account.getUsername());
+    }
+}
